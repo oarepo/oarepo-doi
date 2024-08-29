45955da054e19fecab813d81cb8e8bcfe2b6197d
Alzpetka <lili3.pokorna@seznam.cz>
2024-08-29 13:37:34 +0200

    non duplicable request type

--- oarepo_doi/types/doi.py
+++ oarepo_doi/types/doi.py
@@ -2,8 +2,9 @@ from invenio_requests.customizations import RequestType
 from oarepo_runtime.i18n import lazy_gettext as _
 from ..actions.doi import CreateDoiAction, EditDoiAction
 from oarepo_requests.types.ref_types import ModelRefTypes
+from oarepo_requests.types.generic import NonDuplicableOARepoRequestType
 
-class CreateDoiRequestType(RequestType):
+class CreateDoiRequestType(NonDuplicableOARepoRequestType):
     type_id = "create_doi"
     name = _("create_doi")
 
@@ -15,7 +16,7 @@ class CreateDoiRequestType(RequestType):
     receiver_can_be_none = True
     allowed_topic_ref_types = ModelRefTypes(published=True, draft=True)
 
-class EditDoiRequestType(RequestType):
+class EditDoiRequestType(NonDuplicableOARepoRequestType):
     type_id = "edit_doi"
     name = _("edit_doi")
 
