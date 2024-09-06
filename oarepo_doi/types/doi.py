b68ecb367eb588c402e15588ea4577caeb3894c8
Alzpetka <lili3.pokorna@seznam.cz>
2024-09-06 12:07:16 +0200

    request type parent

--- oarepo_doi/types/doi.py
+++ oarepo_doi/types/doi.py
@@ -1,9 +1,9 @@
-from invenio_requests.customizations import RequestType
+from oarepo_requests.types.generic import NonDuplicableOARepoRequestType
 from oarepo_runtime.i18n import lazy_gettext as _
 from ..actions.doi import CreateDoiAction, ValidateDataForDoiAction
 from oarepo_requests.types.ref_types import ModelRefTypes
 
-class AssignDoiRequestType(RequestType):
+class AssignDoiRequestType(NonDuplicableOARepoRequestType):
     type_id = "assign_doi"
     name = _("assign_doi")
 
@@ -13,5 +13,5 @@ class AssignDoiRequestType(RequestType):
         "submit": ValidateDataForDoiAction,
     }
 
-    receiver_can_be_none = True
+    receiver_can_be_none = False
     allowed_topic_ref_types = ModelRefTypes(published=True, draft=True)
