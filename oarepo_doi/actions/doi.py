695a781c3966b2b72ad76f8a887cc0757d79af4d
Alzbeta Pokorna <lili3.pokorna@seznam.cz>
2025-04-11 11:26:51 +0200

    providers

--- oarepo_doi/actions/doi.py
+++ oarepo_doi/actions/doi.py
@@ -1,27 +1,12 @@
 from flask import current_app
-from invenio_base.utils import obj_or_import_string
 from marshmallow.exceptions import ValidationError
 from oarepo_requests.actions.generic import OARepoAcceptAction, OARepoSubmitAction
 
-from oarepo_doi.api import community_slug_for_credentials, create_doi, delete_doi
+from functools import cached_property
 
-class AssignDoiAction(OARepoAcceptAction):
-    log_event = True
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.mode = current_app.config.get("DATACITE_MODE")
-        self.url = current_app.config.get("DATACITE_URL")
-        self.mapping = current_app.config.get("DATACITE_MAPPING")
-        self.specified_doi = current_app.config.get("DATACITE_SPECIFIED_ID")
-        self.provider = self._provider
-        self.username = None
-        self.password = None
-        self.prefix = None
-
-    @property
-    def _provider(self):
+class OarepoDoiActionMixin:
+    @cached_property
+    def provider(self):
         providers = current_app.config.get("RDM_PERSISTENT_IDENTIFIER_PROVIDERS")
 
         for _provider in providers:
@@ -29,49 +14,30 @@ class AssignDoiAction(OARepoAcceptAction):
                 provider = _provider
                 break
         return provider
-    def credentials(self, community):
-        if not community:
-            credentials = current_app.config.get(
-                "DATACITE_CREDENTIALS_DEFAULT"
-            )
-        else:
-            credentials_def = current_app.config.get("DATACITE_CREDENTIALS")
 
-            credentials = credentials_def.get(community, None)
-            if not credentials:
-                credentials = current_app.config.get(
-                    "DATACITE_CREDENTIALS_DEFAULT"
-                )
+class AssignDoiAction(OARepoAcceptAction, OarepoDoiActionMixin):
+    log_event = True
 
-        self.username = credentials["username"]
-        self.password = credentials["password"]
-        self.prefix = credentials["prefix"]
 
 class CreateDoiAction(AssignDoiAction):
 
 
     def execute(self, identity, uow, *args, **kwargs):
+
         topic = self.request.topic.resolve()
-        slug = community_slug_for_credentials(topic.parent["communities"].get("default", None))
 
-        self.credentials(slug)
-
-        #todo - only public?
         if topic.is_draft:
-            create_doi(self, topic, topic, None)
+            self.provider.create_and_reserve(topic)
         else:
-            create_doi(self, topic, topic, "publish")
+            self.provider.create_and_reserve(topic, event="publish")
         super().execute(identity, uow)
 
 class DeleteDoiAction(AssignDoiAction):
 
     def execute(self, identity, uow, *args, **kwargs):
         topic = self.request.topic.resolve()
-        slug = community_slug_for_credentials(topic.parent["communities"].get("default", None))
 
-        self.credentials(slug)
-
-        delete_doi(self, topic)
+        self.provider.delete(topic)
 
         super().execute(identity, uow)
 
@@ -79,31 +45,13 @@ class RegisterDoiAction(AssignDoiAction):
 
     def execute(self, identity, uow, *args, **kwargs):
         topic = self.request.topic.resolve()
-        slug = community_slug_for_credentials(topic.parent["communities"].get("default", None))
 
-        self.credentials(slug)
-
-        create_doi(self, topic, topic, None)
+        self.provider.create_and_reserve(topic)
 
         super().execute(identity, uow)
-class ValidateDataForDoiAction(OARepoSubmitAction):
+
+class ValidateDataForDoiAction(OARepoSubmitAction, OarepoDoiActionMixin):
     log_event = True
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.provider = self._provider
-
-        self.mapping = current_app.config.get("DATACITE_MAPPING")
-
-    @property
-    def _provider(self):
-        providers = current_app.config.get("RDM_PERSISTENT_IDENTIFIER_PROVIDERS")
-
-        for _provider in providers:
-            if _provider.name == "datacite":
-                provider = _provider
-                break
-        return provider
 
     def execute(self, identity, uow, *args, **kwargs):
         topic = self.request.topic.resolve()
--- oarepo_doi/services/components/__init__.py
+++ oarepo_doi/services/components/__init__.py
@@ -1,100 +1,42 @@
 from flask import current_app
 from invenio_records_resources.services.records.components import ServiceComponent
 
-from oarepo_doi.api import community_slug_for_credentials, create_doi, edit_doi
-from invenio_base.utils import obj_or_import_string
-
 class DoiComponent(ServiceComponent):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.mode = current_app.config.get("DATACITE_MODE")
-        self.url = current_app.config.get("DATACITE_URL")
-        self.mapping = current_app.config.get("DATACITE_MAPPING")
-        self.specified_doi = current_app.config.get("DATACITE_SPECIFIED_ID")
-        self.provider = self._provider
-
-        self.username = None
-        self.password = None
-        self.prefix = None
 
     @property
-    def _provider(self):
+    def provider(self):
         providers = current_app.config.get("RDM_PERSISTENT_IDENTIFIER_PROVIDERS")
-
         for _provider in providers:
             if _provider.name == "datacite":
                 provider = _provider
                 break
         return provider
 
-    def credentials(self, community):
-        if not community:
-            credentials = current_app.config.get(
-                "DATACITE_CREDENTIALS_DEFAULT"
-            )
-        else:
-            credentials_def = current_app.config.get("DATACITE_CREDENTIALS")
-
-            credentials = credentials_def.get(community, None)
-            if not credentials:
-                credentials = current_app.config.get(
-                    "DATACITE_CREDENTIALS_DEFAULT"
-                )
-        self.username = credentials["username"]
-        self.password = credentials["password"]
-        self.prefix = credentials["prefix"]
-
     def create(self, identity, data=None, record=None, **kwargs):
         if self.mode == "AUTOMATIC_DRAFT":
-            slug = community_slug_for_credentials(
-                record.parent["communities"].get("default", None)
-            )
-            self.credentials(slug)
-            create_doi(self, record, data, None)
+            self.provider.create_and_reserve(record)
 
     def update_draft(self, identity, data=None, record=None, **kwargs):
         if self.mode == "AUTOMATIC_DRAFT" or self.mode == "ON_EVENT_DRAFT":
-            slug = community_slug_for_credentials(
-                record.parent["communities"].get("default", None)
-            )
-
-            self.credentials(slug)
-            edit_doi(self, record)
+            self.provider.update(record)
 
     def update(self, identity, data=None, record=None, **kwargs):
-        if (
-            self.mode == "AUTOMATIC_DRAFT"
-            or self.mode == "AUTOMATIC"
-            or self.mode == "ON_EVENT"
-            or self.mode == "ON_EVENT_DRAFT"
-        ):
-            slug = community_slug_for_credentials(
-                record.parent["communities"].get("default", None)
-            )
-            self.credentials(slug)
-            edit_doi(self, record)
+        self.provider.update(record)
 
     def publish(self, identity, data=None, record=None, draft=None, **kwargs):
-        record.pids = draft.pids #todo because of excluded pids components pids are not in published record, this needs to be solved better
         if record.pids is None:
             record.pids = {}
         if self.mode == "AUTOMATIC":
-            slug = community_slug_for_credentials(
-                record.parent["communities"].get("default", None)
-            )
-            self.credentials(slug)
-            create_doi(self, record, record, "publish")
+            self.provider.create_and_reserve(record,  event = "publish")
         if self.mode == "AUTOMATIC_DRAFT" or self.mode == "ON_EVENT_DRAFT":
-            slug = community_slug_for_credentials(
-                record.parent["communities"].get("default", None)
-            )
-            self.credentials(slug)
-            edit_doi(self, record, "publish")
+            self.provider.update(record, event ="publish")
 
     def new_version(self, identity, draft=None, record=None, **kwargs):
         """Update draft metadata."""
-        mapping = obj_or_import_string(self.mapping[record.schema])()
-        doi_value = mapping.get_doi_value(record)
+        doi_value = self.provider.get_doi_value(record)
         if doi_value is not None:
-            mapping.remove_doi_value(draft)
+            self.provider.remove_doi_value(draft)
