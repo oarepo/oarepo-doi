ccd382d5fdfa2cfa381f4056479946dc35c1905e
Alzbeta Pokorna <lili3.pokorna@seznam.cz>
2025-06-25 10:58:41 +0200

    add builders to app config

--- oarepo_doi/ext.py
+++ oarepo_doi/ext.py
@@ -1,3 +1,6 @@
+from .config import NOTIFICATIONS_BUILDERS
+from deepmerge import conservative_merger
+
 class OARepoDOI(object):
     """OARepo DOI extension."""
 
@@ -19,3 +22,10 @@ class OARepoDOI(object):
             app.config["DATACITE_MODE"] = "ON_EVENT"
         if "DATACITE_SPECIFIED_ID" not in app.config:
             app.config["DATACITE_SPECIFIED_ID"] = False
+
+        app_notification_builders = app.config.setdefault(
+            "NOTIFICATIONS_BUILDERS", {}
+        )
+        app.config["NOTIFICATIONS_BUILDERS"] = conservative_merger.merge(
+            app_notification_builders, NOTIFICATIONS_BUILDERS
+        )
