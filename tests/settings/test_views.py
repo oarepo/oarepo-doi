303b4f073d6246aeb49680eb436d359cb2f1603e
Alzbeta Pokorna <lili3.pokorna@seznam.cz>
2026-08-13 13:55:57 +0200

    test: doi settings

---
+++ tests/settings/test_views.py
@@ -0,0 +1,25 @@
+#
+# Copyright (c) 2026 CESNET z.s.p.o.
+#
+# This file is a part of oarepo-doi (see http://github.com/oarepo/oarepo-doi).
+#
+# oarepo-runtime is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""Tests for DOI settings views."""
+
+from __future__ import annotations
+
+from types import SimpleNamespace
+from unittest.mock import Mock
+
+from oarepo_doi.settings.views import create_api_blueprint
+
+
+def test_create_api_blueprint_uses_doi_settings_resource(app):
+    """API blueprint is created from the DOI settings resource."""
+    blueprint = Mock()
+    doi_settings_resource = SimpleNamespace(as_blueprint=Mock(return_value=blueprint))
+    app.extensions["doi-settings"] = SimpleNamespace(doi_settings_resource=doi_settings_resource)
+
+    assert create_api_blueprint(app) is blueprint
